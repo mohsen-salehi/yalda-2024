46cb599a9f9775ded1a7e57d49404f48c94238d6
mohsen salehi <mohsensalehi2001@gmail.com>
2023-12-16 18:53:56 +0330

    change image

--- components/form/username&password.js
+++ components/form/username&password.js
@@ -6,21 +6,18 @@ import requestAction from "../../redux/actions/auth/requestAction";
 
 function UsernamePassword() {
 
-    const fullName = useRef()
     const phoneNumber = useRef()
     const dispatch = useDispatch()
     const formHandler = (e) => {
         e.preventDefault()
-        if (fullName.current?.value.length >= 2 && phoneNumber.current?.value.length === 11 ) {
+        if (phoneNumber.current?.value.length === 11 ) {
             dispatch(formAction({
-                fullName : fullName.current?.value,
                 mobile : phoneNumber.current?.value
             }))
             dispatch(requestAction({contact : phoneNumber.current?.value}))
         }else {
             toast.error(`
-            فیلد نام و نام خانوادگی خالی نباشد و
-            شماره تلفن باید 11 رقم باشد
+            فیلد شماره باید ۱۱ رقم باشد .
             `)
         }
     }
@@ -34,11 +31,6 @@ function UsernamePassword() {
             </h2>
             <form onSubmit={formHandler} dir="rtl"
                   className="col-12 p-2  rounded d-flex justify-content-center align-content-between flex-wrap">
-                <label className="col-12 d-flex justify-content-center align-items-center my-2">
-                    <input ref={fullName} type="text" name="fullName" id="fullName"
-                           className="border shadow-sm col-11 col-xl-5 rounded p-2 bg-light fw-bold"
-                           placeholder="نام و نام خانوادگی"/>
-                </label>
                 <label className="col-12 d-flex justify-content-center align-items-center my-2">
                     <input maxLength="11" ref={phoneNumber} type="text" name="phone" id="phone"
                            className="border shadow-sm col-11 col-xl-5 rounded p-2 bg-light fw-bold"
--- components/popup/index.js
+++ components/popup/index.js
@@ -1,12 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import {useSelector} from "react-redux";
 import {toast} from "react-toastify";
+import Image from "next/image";
 
 function Index({setStatusPopup, setStatusInvite}) {
     const [number , setNumber] = useState(0)
     const {request} = useSelector(state => state)
-    const {data} = useSelector(state => state.verify.user)
-    const verify = useSelector(state => state.verify)
     useEffect(()=>{
         setNumber(Math.floor(Math.random() * 10) + 1)
     },[request])
@@ -15,72 +14,11 @@ function Index({setStatusPopup, setStatusInvite}) {
         <div className="section  col-11 p-0 col-xl-4 h-75 d-flex justify-content-center align-content-center flex-wrap ">
             <figure
                 className="col-11 col-md-5 col-xl-9  d-flex flex-wrap justify-content-center align-items-center position-relative">
-                <img src={`/img/fal${number}.png`} className=" overflow-hidden" alt=""/>
-
-
-                <div className="col-12 d-flex justify-content-center align-items-center flex-wrap position-absolute bottom-0  mb-0 mb-xl-3 font-mobile">
-
-                    {
-                        request.data ? (
-                            request.data && (
-                                request?.data?.vouchers_by_tag?.map((item , index) => (
-                                    <div key={index} className="col-11 d-flex justify-content-between align-items-center p-1 bg-white p-xl-2 mt-1 shadow-sm rounded-pill">
-                                        <button className="col-2 btn btn-danger rounded-pill text-white fw-bold py-2 font-mobile" onClick={(e) => {
-                                            navigator?.clipboard?.writeText(JSON.parse(item?.voucher.info)?.code)
-                                            toast.success('کد تخفیف شما کپی شد')
-                                        } }>کپی
-                                        </button>
-                                        <strong dir="rtl"
-                                            className="col-8 d-flex justify-content-center text-center  align-items-center border border-2 rounded-pill border-danger  py-2 mx-1">
-                                            {JSON.parse(item?.voucher.info)?.title}
-                                        </strong>
-                                        <div className="col-2 d-flex justify-content-center align-items-center ">
-                                            <span className="text-danger d-flex justify-content-center align-items-center fw-bold">
-                                                هدیه
-                                                {
-                                                    index === 0 ? ' اول'
-                                                        : index === 1 ? " دوم "
-                                                            : " سوم "
-                                                }
-                                            </span>
-                                        </div>
-                                    </div>
-                                ))
-                            )
-                        ) : (
-                            verify.status  ?
-                                data?.data?.vouchers_by_tag?.map((item , index) => (
-                                    <div key={index} className="col-11 d-flex justify-content-between align-items-center p-1 bg-white p-lg-2 mt-1 shadow-sm rounded-pill">
-                                        <button className="col-2 btn btn-danger rounded-pill text-white fw-bold py-2 font-mobile" onClick={(e) => {
-                                            navigator?.clipboard?.writeText(JSON.parse(item?.voucher.info)?.code)
-                                            toast.success('کد تخفیف شما کپی شد')
-                                        } }>کپی
-                                        </button>
-                                        <strong dir="rtl"
-                                            className="col-8 d-flex justify-content-center text-center  align-items-center border border-2 rounded-pill border-danger  py-2 mx-1">
-                                            {JSON.parse(item?.voucher.info)?.title}
-                                        </strong>
-                                        <div className="col-2 d-flex justify-content-center align-items-center ">
-                                            <span className="text-danger d-flex justify-content-center align-items-center fw-bold">
-                                                هدیه
-                                                {
-                                                    index === 0 ? ' اول '
-                                                        : index === 1 ? " دوم "
-                                                            : " سوم "
-                                                }
-                                            </span>
-                                        </div>
-                                    </div>
-                                ))
-                                :''
-                        )
-                    }
-
-                </div>
+                <Image src={`/img/fal${number}.png`} width={500} height={600}  className="overflow-hidden" alt=""/>
             </figure>
 
             <div
-                className="col-11 col-xl-9 d-flex justify-content-center align-items-center bg-light p-2 rounded-4 bg-opacity-50 z-index-2">
+                className="col-11 col-xl-9  d-flex justify-content-center align-items-center bg-light p-2 rounded-4 bg-opacity-50 z-index-2">
                 <button onClick={() => setStatusPopup(false)}
                         className="btn col-xl-5 btn-danger mx-1 d-flex justify-content-center align-items-center">
                     بستن
